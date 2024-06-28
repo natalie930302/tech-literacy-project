97e6841870b84edded070b890dbac9fb57651240
Natalie Hsu <78529168+natalie930302@users.noreply.github.com>
2024-06-29 04:41:06 +0800

    feat

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -45,42 +45,6 @@ export default function Header() {
                     >
                       師培/專業課程
                     </Link>
-                    <div className="absolute top-full left-0 scale-0 group-hover:scale-100 pt-3">
-                      <ul className="flex flex-col gap-2 w-max min-w-32 bg-white/95 text-gray-500 rounded shadow-default overflow-hidden p-3">
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            科技領域教材教法
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            科技素養與倫理
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            科技教育概論
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            創意與設計專題
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
                   </li>
                   <li className="relative group">
                     <Link
@@ -89,50 +53,6 @@ export default function Header() {
                     >
                       科技領域專長
                     </Link>
-                    <div className="absolute top-full left-0 scale-0 group-hover:scale-100 pt-3">
-                      <ul className="flex flex-col gap-2 w-max min-w-32 bg-white/95 text-gray-500 rounded shadow-default overflow-hidden p-3">
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            校內法規修訂
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            次專長加註辦法
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            科技領域微學程
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            專長加註學程說明會
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            科技領域研習講座
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
                   </li>
                   <li className="relative group">
                     <Link
@@ -141,42 +61,6 @@ export default function Header() {
                     >
                       實習資源
                     </Link>
-                    <div className="absolute top-full left-0 scale-0 group-hover:scale-100 pt-3">
-                      <ul className="flex flex-col gap-2 w-max min-w-32 bg-white/95 text-gray-500 rounded shadow-default overflow-hidden p-3">
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            區域科技中心
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            國內實習學校
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            兒童與多元活動
-                          </Link>
-                        </li>
-                        <li>
-                          <Link
-                            href="#"
-                            className="w-fit text-xs border-y-2 border-transparent hover:border-b-denim-300 block whitespace-no-wrap px-2 py-1"
-                          >
-                            三週實習輔導
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
                   </li>
                 </ul>
               </div>
