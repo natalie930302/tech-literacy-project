6396a940dc8282ec792190379c4478ea32f23ad7
Natalie Hsu <78529168+natalie930302@users.noreply.github.com>
2024-09-12 17:56:52 +0800

    style

--- src/components/expertise-list/expertise-list.tsx
+++ src/components/expertise-list/expertise-list.tsx
@@ -13,11 +13,11 @@ const ExpertiseList: React.FC = async () => {
       <div className="container py-8 md:py-16">
         <div className="flex flex-col gap-4 md:gap-8">
           <h2 className="font-semibold text-2xl md:text-3xl">國小教師加註科技領域專長通過名單</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             {Expertise.map((expertise, index) => (
               <div key={index} className="flex gap-4 text-denim-700 py-2 px-4 border">
-                <div>{expertise.Name}</div>
-                <div>{expertise.ID}</div>
+                <div className="whitespace-nowrap">{expertise.Name}</div>
+                <div className="whitespace-nowrap">{expertise.ID}</div>
               </div>
             ))}
           </div>
